5b92786230d4e6fbf4ebc737dc5149c4252ac54b
ulanid <116159219+ulanid@users.noreply.github.com>
2024-05-14 15:38:06 +0300

    Add files via upload

--- builder/Product.java
+++ builder/Product.java
@@ -1,23 +1,42 @@
 public class Product {
-    private final String atribute1;
-    private final int atribute2;
-    private final boolean atribute3;
+    private String name;
+    private int price;
 
-    public Product(Builder builder) {
-        this.atribute1 = builder.getAtribute1();
-        this.atribute2 = builder.getAtribute2();
-        this.atribute3 = builder.isAtribute3();
+    private Product(Builder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
     }
 
-    public String getAtribute1() {
-        return atribute1;
+    public static class Builder {
+        private String name;
+        private int price;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setPrice(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
     }
 
-    public int getAtribute2() {
-        return atribute2;
+    @Override
+    public String toString() {
+        return "Product{name='" + name + "', price=" + price + "}";
     }
 
-    public boolean isAtribute3() {
-        return atribute3;
+    public static void main(String[] args) {
+        Product product = new Product.Builder()
+            .setName("Laptop")
+            .setPrice(1500)
+            .build();
+
+        System.out.println(product);
     }
 }
